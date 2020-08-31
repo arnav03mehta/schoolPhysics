a4ee5bb072b66a04fefeb0ff10faa681128faecd
Arnav Mehta <arnav2003mehta@gmail.com>
2020-09-01 01:07:13 +0530

    created better graphs

--- motion.py
+++ motion.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 class thing :
+    
     '''
       Created a class called thing, which will be the object you throw.
       To use this, you first have to create an object, which you do as follows :
@@ -17,13 +18,19 @@ class thing :
       or
           thingName.throw_from_ground(velocity,angle)
     '''
+
+    g = 9.8
     def __init__(self,mass=0.001) :
         self.mass = mass 
-        self.weight = mass*9.8
-
-
+        self.weight = mass*self.g
 
     def throw_from_ground(self,velocity,angle_in_deg=0,angle_in_rad=0) :
+        g = self.g
+
+        if angle_in_deg :
+            legend_ =  f'{velocity}m/s, {angle_in_deg} deg'
+        else :
+            legend_ =  f'{velocity}m/s, {angle_in_rad} rad'
 
         '''
           This method will simulate throwing the initialised thing from groung up.
@@ -37,27 +44,34 @@ class thing :
         else :
             angle = angle_in_rad
         x_pos = lambda t : velocity*math.cos(angle) * t
-        y_pos = lambda t : (velocity*math.sin(angle)*t)-((9.8*(t**2))/2)
-        T = 2*velocity*math.sin(angle) / 9.8
+        y_pos = lambda t : (velocity*math.sin(angle)*t)-((g*(t**2))/2)
+        T = 2*velocity*math.sin(angle) / g
+
         xList = [ x_pos(i) for i in np.arange(0,T,0.01) ]
         yList = [ y_pos(i) for i in np.arange(0,T,0.01) ]
-        max_height = (velocity**2)*math.sin(2*angle)/9.8
-        max_dist = (velocity**2)*(math.sin(angle)**2)/(2*9.8)
-        plt.plot(xList,yList)
+
+        max_height = (velocity**2)*math.sin(2*angle)/g
+        max_dist = (velocity**2)*(math.sin(angle)**2)/(2*g)
         
         print()
         print(f"Total time in air(in seconds) : {T}")
         print(f"Max horizontal distance(in meters) : { max_height }")
         print(f"Max height reached(in meters) : { max_dist }")
         
+        plt.plot(xList,yList)
+        plt.xlabel('---------- x -------->')
+        plt.ylabel('---------- y -------->')
+        plt.grid(True)
+
         if max_height > max_dist :
             plt.ylim([0,max_height])
         else :
             plt.xlim([0,max_dist])
-
+        plt.legend([legend_])
         plt.show()
 
     def throw_horizontally(self,velocity,height) :
+        g = self.g
 
         '''
           This method is similar in functionality to the above one,
@@ -67,8 +81,8 @@ class thing :
         '''
 
         x_pos = lambda t : velocity * t
-        y_pos = lambda t : height - ((9.8*(t**2))/(2*(velocity**2))) 
-        T = (2*height/9.8)**(1/2)
+        y_pos = lambda t : height - ((g*(t**2))/(2*(velocity**2))) 
+        T = (2*height/g)**(1/2)
 
         xList = [ x_pos(i) for i in np.arange(0,T,0.0001) ]
         yList = [ y_pos(i) for i in np.arange(0,T,0.0001) ]
@@ -79,13 +93,20 @@ class thing :
         print(f"Max horizontal distance : { max_dist }")
         
         plt.plot(xList,yList)
+        plt.xlabel('---------- x -------->')
+        plt.ylabel('---------- y -------->')
+        plt.grid(True)
+        
         '''
           < Not implemented yet, just ignore >
+          
         if height > max_dist :
             plt.xlim([0,height])
         else :
             plt.ylim([0,max_dist])
         '''
+
+        plt.legend([f'{velocity}m/s, {height}m'])
         plt.show()
 
 
@@ -99,3 +120,9 @@ if __name__ == "__main__":
     v = 5 # int(input("how much initial velocity(in m/s) do you want? : "))
     h = 10 # int(input("from what height(in meters) you want to throw at? : "))
     theObject.throw_horizontally(v,h)
+
+
+
+
+
+# %%
